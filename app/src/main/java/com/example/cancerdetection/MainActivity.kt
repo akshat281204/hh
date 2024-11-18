9fef9e76bd5120ab603132bbc4ac3329d43deb95
akshat281204 <akshatagarwal281204@gmail.com>
2024-11-18 15:06:07 +0530

    finished UI of signup/login activities

---
+++ app/src/main/java/com/example/cancerdetection/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.cancerdetection
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun setContentView(layoutResID: Int) {
+        super.setContentView(R.layout.activity_main)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val t1=findViewById<TextView>(R.id.sign)
+        t1.setOnClickListener{
+            val exp_signup= Intent(this,MainActivity2::class.java)
+            startActivity(exp_signup)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cancerdetection/MainActivity2.kt
@@ -0,0 +1,32 @@
+package com.example.cancerdetection
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.cancerdetection.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val t2=findViewById<TextView>(R.id.log)
+        t2.setOnClickListener{
+            this.finish()
+        }
+    }
+}
