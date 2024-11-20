4912881f239fabe4b862bf5ff6e278cf184d1885
akshat281204 <akshatagarwal281204@gmail.com>
2024-11-20 18:39:06 +0530

    First Commit

--- app/src/main/java/com/example/cancerdetection/MainActivity.kt
+++ app/src/main/java/com/example/cancerdetection/MainActivity.kt
@@ -2,19 +2,31 @@ package com.example.cancerdetection
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cancerdetection.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private var db=Firebase.firestore
     override fun setContentView(layoutResID: Int) {
         super.setContentView(R.layout.activity_main)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth=FirebaseAuth.getInstance()
+        val db = Firebase.firestore
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -22,10 +34,48 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val emailfield=findViewById<EditText>(R.id.email_log)
+        val passfield=findViewById<EditText>(R.id.password_log)
+        val button_log=findViewById<Button>(R.id.button_log)
+
+        button_log.setOnClickListener{
+            val email=emailfield.text.toString()
+            val password=passfield.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                login(email,password)
+            }
+            else{
+                Toast.makeText(this,"Please enter email and password",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
         val t1=findViewById<TextView>(R.id.sign)
         t1.setOnClickListener{
             val exp_signup= Intent(this,MainActivity2::class.java)
             startActivity(exp_signup)
+        }
+    }
+    private fun login(email: String,password: String){
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener{ task ->
+            if(task.isSuccessful){
+                // FETCHING USER NAME
+                val userid=FirebaseAuth.getInstance().currentUser!!.uid
+                val ref=db.collection("user").document(userid)
+                ref.get().addOnSuccessListener{
+                    if(it!=null){
+                        val name= it.data?.get("name")?.toString()
+                        Toast.makeText(this,"Welcome $name",Toast.LENGTH_SHORT).show()
+                        val home_intent=Intent(this,Home_act::class.java)
+                        startActivity(home_intent)
+                    }
+                }
+
+            }
+            else{
+                Toast.makeText(this,"Incorrect Credentials",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/cancerdetection/MainActivity2.kt
+++ app/src/main/java/com/example/cancerdetection/MainActivity2.kt
@@ -2,7 +2,12 @@ package com.example.cancerdetection
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -13,10 +18,27 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.cancerdetection.databinding.ActivityMain2Binding
+import com.example.cancerdetection.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+data class User(
+    val name: String,
+    val dob: String,
+    val phone: String,
+    val email: String,
+    val password: String
+)
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private var db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth=FirebaseAuth.getInstance()
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -24,9 +46,80 @@ class MainActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val unfield=findViewById<EditText>(R.id.username_su)
+        val dobfield=findViewById<EditText>(R.id.dob_su)
+        val phonefield=findViewById<EditText>(R.id.phone_su)
+        val emailfield=findViewById<EditText>(R.id.email_su)
+        val passfield=findViewById<EditText>(R.id.password_su)
+        val but_su=findViewById<Button>(R.id.button_su)
+        val back=findViewById<FloatingActionButton>(R.id.back2)
+
+        back.setOnClickListener{
+            finish()
+        }
+
+        but_su.setOnClickListener{
+            val name=unfield.text.toString()
+            val dob=dobfield.text.toString()
+            val phone=phonefield.text.toString()
+            val email=emailfield.text.toString()
+            val password=passfield.text.toString()
+
+            if(name.isNotEmpty() && dob.isNotEmpty() && phone.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()){
+                register(name,dob,phone,email,password)
+            }
+            else{
+                Toast.makeText(this,"Please fill all the fields",Toast.LENGTH_SHORT).show()
+            }
+        }
+
         val t2=findViewById<TextView>(R.id.log)
         t2.setOnClickListener{
             this.finish()
+        }
+    }
+    // REGISTER EMAIL AND PASSWORD FOR LOGIN
+    fun register(name: String, dob: String, phone:String, email: String, password: String){
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{
+            if(it.isSuccessful){
+                // ADD VALUE TO THE FIRESTORE DATABASE
+                val userMap= hashMapOf(
+                    "name" to name,
+                    "dob" to dob,
+                    "phone_no" to phone,
+                    "email" to email
+                )
+                val userId=auth.currentUser?.uid ?:"unknown_id"
+                db.collection("user").document(userId).set(userMap)
+                    .addOnCompleteListener{
+                        if(it.isSuccessful){
+                            Toast.makeText(this, "User Registered Successfully", Toast.LENGTH_SHORT).show()
+                        }
+                        else{
+                            Toast.makeText(this, "User Not Registered", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    .addOnFailureListener{exception->
+                        Toast.makeText(this,"Error: ${exception.message}",Toast.LENGTH_LONG).show()
+                    }
+                val userid=auth.currentUser?.uid
+                val database=FirebaseDatabase.getInstance().getReference("users")
+                val user=User(name,dob,phone,email,password)
+                userid?.let{database.child(it).setValue(user)}?.addOnCompleteListener{dbtask ->
+                    if(dbtask.isSuccessful){
+                        Toast.makeText(this,"Registration Complete",Toast.LENGTH_SHORT).show()
+                        val home_intent=Intent(this, Home_act::class.java)
+                        startActivity(home_intent)
+                    }
+                    else{
+                        Toast.makeText(this,"Failed to save user data",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else{
+                Toast.makeText(this,"Registration failed",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
